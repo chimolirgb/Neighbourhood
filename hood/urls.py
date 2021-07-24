ed3c8d0787a8c227538d2dce4f208b6b00093807
Moringa School <chimolilucy@gmail.com>
2021-07-24 15:26:56 +0300

    adding category and welcome path in urls.py

--- hood/urls.py
+++ hood/urls.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
+
+    path('',views.welcome,name = 'welcome'),
+
     path(r'^create/profile/$',views.create_profile, name='create-profile'),
     path('profile/<str:username>/',views.profile,name='profile'),
     path(r'^api/profiles/$', views.ProfileList.as_view()),
@@ -11,4 +14,5 @@ urlpatterns=[
     path('category/',views.category,name='category'),
 
 ]
-    
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
