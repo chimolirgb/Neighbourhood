88a56802cf92a9e1053093f6739e6a2b1a460031
Moringa School <chimolilucy@gmail.com>
2021-07-24 13:38:18 +0300

    creating registration base.html

--- hood/urls.py
+++ hood/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-path(r'^create/profile/$',views.create_profile, name='create-profile'),
+    path(r'^create/profile/$',views.create_profile, name='create-profile'),
     path('profile/<str:username>/',views.profile,name='profile'),
     path(r'^api/profiles/$', views.ProfileList.as_view()),
+
+    path('category/',views.category,name='category'),
 
 ]
     
